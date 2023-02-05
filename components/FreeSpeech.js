1765b5e2dd6c0305c594070b84dd433ce5f982ba
ufelgen <felgenhauer.u@gmail.com>
2023-02-05 19:49:22 +0100

    move code up to index.js and add state for currently speaking

--- components/FreeSpeech.js
+++ components/FreeSpeech.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
-import { useState } from "react";
-import useLocalStorageState from "use-local-storage-state";
-import { useSpeechSynthesisApi } from "@/hooks/WebSpeech";
+
 import PresetButtons from "./PresetButtons";
 import CustomButtons from "./CustomButtons";
 import {
@@ -11,98 +9,63 @@ import {
   SecondSection,
 } from "./StylesForSpeech";
 
-export default function FreeSpeech() {
-  const {
-    text,
-    setText,
-    isSpeaking,
-    isPaused,
-    isResumed,
-    isEnded,
-    speak,
-    pause,
-    resume,
-    cancel,
-  } = useSpeechSynthesisApi();
-
-  const [input, setInput] = useState("preset");
-  const [customButtons, setCustomButtons] = useLocalStorageState(
-    "customButtons",
-    {
-      defaultValue: [],
-    }
-  );
-  const [buttonForm, setButtonForm] = useState(false);
-
-  /*   if (typeof window !== "undefined") {
-    const synth = window.speechSynthesis;
-    const amISpeaking = synth.speaking;
-    return amISpeaking;
-  }
- */
-  function handleToggleButtonForm() {
-    setButtonForm(!buttonForm);
-  }
-
-  function handleSpeak(message) {
-    let msg = new SpeechSynthesisUtterance();
-
-    msg.text = message;
-    msg.lang = "en";
-    /*     msg.voice = voices?.filter(function (voice) {
-      return voice.name == "Google UK English Male";
-    })[0]; */
-    function speak() {
-      window.speechSynthesis.speak(msg);
-    }
-    speak();
-  }
-
-  function handleNewButton(newButton) {
-    setCustomButtons([...customButtons, newButton]);
-  }
-
-  function handleDeleteButton(buttonId) {
-    const confirmation = confirm("do you want to delete this phrase?");
-    if (confirmation) {
-      setCustomButtons(
-        customButtons.filter((button) => button.id !== buttonId)
-      );
-    }
-  }
+export default function FreeSpeech({
+  text,
+  onSetText,
+  isSpeaking,
+  isPaused,
+  isResumed,
+  isEnded,
+  speak,
+  pause,
+  resume,
+  cancel,
+  onSetInput,
+  onSetButtonForm,
+  onSetCurrentlySpeaking,
+  onSetCustomButtons,
+  onToggleButtonForm,
+  onSpeak,
+  onNewButton,
+  onDeleteButton,
+  customButtons,
+  buttonForm,
+  input,
+}) {
   return (
     <>
       <Container>
         <SelectButtonSection>
           <button
-            onClick={() => setInput("preset")}
+            onClick={() => onSetInput("preset")}
             className={input === "preset" ? "current" : ""}
           >
             preset
           </button>
           <button
-            onClick={() => setInput("custom")}
+            onClick={() => onSetInput("custom")}
             className={input === "custom" ? "current" : ""}
           >
             custom
           </button>
           <button
-            onClick={() => setInput("freestyle")}
+            onClick={() => onSetInput("freestyle")}
             className={input === "freestyle" ? "current" : ""}
           >
             freestyle
           </button>
         </SelectButtonSection>
-        {input === "preset" && <PresetButtons onSpeak={handleSpeak} />}
+        {input === "preset" && <PresetButtons onSpeak={onSpeak} />}
 
         {input === "custom" && (
           <CustomButtons
-            onToggleButtonForm={handleToggleButtonForm}
-            onSpeak={handleSpeak}
-            onAddNewButton={handleNewButton}
+            onToggleButtonForm={onToggleButtonForm}
+            onSpeak={onSpeak}
+            onNewButton={onNewButton}
             customButtons={customButtons}
             buttonForm={buttonForm}
-            onDeleteButton={handleDeleteButton}
+            onDeleteButton={onDeleteButton}
+            onSetInput={onSetInput}
           />
         )}
 
@@ -111,13 +74,15 @@ export default function FreeSpeech() {
             value={text}
             cols={35}
             rows={10}
-            onChange={(event) => setText(event.target.value)}
+            onChange={(event) => onSetText(event.target.value)}
           />
         )}
 
         <StyledCommands>
           <SecondSection>
-            {input === "freestyle" && <button onClick={speak}>speak</button>}
+            {input === "freestyle" && (
+              <button onClick={() => onSpeak(text)}>speak</button>
+            )}
             <button onClick={pause}>pause</button>
             <button onClick={resume}>resume</button>
             <button onClick={cancel}>cancel</button>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,113 @@
 import Header from "@/components/Header";
 import FreeSpeech from "@/components/FreeSpeech";
 import RobotFace from "@/components/RobotFace";
+import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+import { useSpeechSynthesisApi } from "@/hooks/WebSpeech";
 
 export default function Home() {
+  const {
+    text,
+    setText,
+    isSpeaking,
+    isPaused,
+    isResumed,
+    isEnded,
+    speak,
+    pause,
+    resume,
+    cancel,
+  } = useSpeechSynthesisApi();
+
+  const [input, setInput] = useState("preset");
+  const [customButtons, setCustomButtons] = useLocalStorageState(
+    "customButtons",
+    {
+      defaultValue: [],
+    }
+  );
+  const [buttonForm, setButtonForm] = useState(false);
+  const [currentlySpeaking, setCurrentlySpeaking] = useState(false);
+
+  function onSetInput(input) {
+    setInput(input);
+  }
+  function onSetButtonForm(buttonForm) {
+    setButtonForm(buttonForm);
+  }
+  function onSetCustomButtons(customButtons) {
+    setCustomButtons(customButtons);
+  }
+  function onSetCurrentlySpeaking(currentlySpeaking) {
+    setCurrentlySpeaking(currentlySpeaking);
+  }
+  function handleToggleButtonForm() {
+    setButtonForm(!buttonForm);
+  }
+
+  function handleSpeak(message) {
+    let msg = new SpeechSynthesisUtterance();
+
+    msg.text = message;
+    msg.lang = "en";
+    /*     msg.voice = voices?.filter(function (voice) {
+      return voice.name == "Google UK English Male";
+    })[0]; */
+    function speak() {
+      window.speechSynthesis.speak(msg);
+    }
+    msg.onstart = (event) => {
+      setCurrentlySpeaking(true);
+      console.log("start");
+    };
+
+    msg.onend = (event) => {
+      setCurrentlySpeaking(false);
+      console.log("end");
+    };
+    speak();
+  }
+
+  function handleNewButton(newButton) {
+    setCustomButtons([...customButtons, newButton]);
+  }
+
+  function handleDeleteButton(buttonId) {
+    const confirmation = confirm("do you want to delete this phrase?");
+    if (confirmation) {
+      setCustomButtons(
+        customButtons.filter((button) => button.id !== buttonId)
+      );
+    }
+  }
+
   return (
     <>
       <Header />
       <RobotFace />
-      <FreeSpeech />
+      <FreeSpeech
+        text={text}
+        onSetText={setText}
+        isSpeaking={isSpeaking}
+        isPaused={isPaused}
+        isResumed={isResumed}
+        isEnded={isEnded}
+        speak={speak}
+        pause={pause}
+        resume={resume}
+        cancel={cancel}
+        onSetInput={onSetInput}
+        onSetButtonForm={onSetButtonForm}
+        onSetCurrentlySpeaking={onSetCurrentlySpeaking}
+        onSetCustomButtons={onSetCustomButtons}
+        onToggleButtonForm={handleToggleButtonForm}
+        onSpeak={handleSpeak}
+        onNewButton={handleNewButton}
+        onDeleteButton={handleDeleteButton}
+        customButtons={customButtons}
+        buttonForm={buttonForm}
+        input={input}
+      />
     </>
   );
 }
